279928b5fc39385f09c4778448d42664d7654329
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 18:29:58 -0500

    Create verificacion-nro11.py

---
+++ verificacion-nro11.py
@@ -0,0 +1,21 @@
+#calculadora nro11
+#hallar el volumen del prisma
+
+#declaraciones de variables
+largo,ancho,altura,volumen_prisma=0.0,0.0,0.0,0.0
+
+#calculadora
+largo=17.76
+ancho=3.68
+altura=20.34
+volumen_prisma=largo*ancho*altura
+
+#mostrar datos
+print("el largo es:", largo)
+print("el ancho es:", ancho)
+print("la altura es:", altura)
+print("el volumen del prisma es:", volumen_prisma)
+
+#verificacion
+menor_igual=largo<=altura
+print("el largo es menor igual a la altura", menor_igual)
