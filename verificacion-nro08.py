2c6426e030e5f5c1ddf53243478f5370fe25cfd0
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 18:09:14 -0500

    Create verificacion-nro08.py

---
+++ verificacion-nro08.py
@@ -0,0 +1,20 @@
+#calculadora nro8
+#calcular el area de un elipse
+
+#declaraciones de variables
+pi,semieje_menor,semieje_mayor,area_elipse=0.0,0.0,0.0,0.0
+
+#calculadora
+pi=3.1416
+semieje_menor=45.54
+semieje_mayor=61.93
+area_elipse=pi*semieje_mayor*semieje_menor
+
+#mostrar datos
+print("el semieje menor es:", semieje_menor)
+print("el semieje mayor es:", semieje_mayor)
+print("el area del elipse es:", area_elipse)
+
+#verificacion
+menor=semieje_mayor<semieje_menor
+print("el semieje mayor es menor al semieje menor", menor)
