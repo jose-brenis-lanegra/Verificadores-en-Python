7f844a7f61acb28556aeec05e33ed4610e415f31
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 17:26:02 -0500

    Create verificacion-nro01.py

---
+++ verificacion-nro01.py
@@ -0,0 +1,19 @@
+#calculadora nro1
+#hallar el valor del area del triangulo
+
+#declaracion de variables
+base,altura,area_triangulo=0.0,0.0,0.0
+
+#calculadora
+base=5.16
+altura=7.54
+area_triangulo=(base*altura)/2
+
+#mostrar datos
+print("la base es:", base)
+print("la altura es:", altura)
+print("el area del triangulo es:", area_triangulo)
+
+#verificacion
+es_mayor=base>area_triangulo
+print("es mayor:",es_mayor)
