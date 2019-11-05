643a27dcecc7706c9f7b80dd81e42b01eafb7269
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 20:02:11 -0500

    Create conversion-cadena.py

---
+++ conversion-cadena.py
@@ -0,0 +1,49 @@
+#convertir la cadena 3.87 a cadena
+x="3.87"
+a=str(x)
+print(a)
+
+#convertir el flotante 34.78 a cadena
+x=34.78
+a=str(x)
+print(a)
+
+#convertir el entero 324 a cadena
+x=324
+a=str(x)
+print(a)
+
+#convertir el booleano 476>=587 a cadena
+x=476>=587
+a=str(x)
+print(a)
+
+#convertir el booleano 64<67 a cadena
+x=64<67
+a=str(x)
+print(a)
+
+#convertir el entero -745 a cadena
+x=-745
+a=str(x)
+print(a)
+
+#convertir el flotante 0.56 a cadena
+x=0.56
+a=str(x)
+print(a)
+
+#convertir la cadena cinco a cadena
+x="cinco"
+a=str(x)
+print(a)
+
+#convertir la cadena 3276!=3276 a cadena
+x="3276!=3276"
+a=str(x)
+print(a)
+
+#convertir el flotante 64.78 a cadena
+x=64.78
+a=str(x)
+print(a)
