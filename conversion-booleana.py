a85d7c9eece3a963f328302149b514646f6e2e26
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 20:14:31 -0500

    Create conversion-booleana.py

---
+++ conversion-booleana.py
@@ -0,0 +1,49 @@
+#convertir la cadena 456.67 en booleano
+x="456.67"
+a=bool(x)
+print(a)
+
+#convertir el entero 456.67 en booleano
+x=456.67
+a=bool(x)
+print(a)
+
+#convertir el entero 1 en booleano
+x=1
+a=bool(x)
+print(a)
+
+#convertir el entero 0 en booleano
+x=0
+a=bool(x)
+print(a)
+
+#convertir el booleano 23==32 en booleano
+x=23==32
+a=bool(x)
+print(a)
+
+#convertir la cadena -30 en booleano
+x="-30"
+a=bool(x)
+print(a)
+
+#convertir la cadena vacia en booleano
+x=""
+a=bool(x)
+print(a)
+
+#convertir la cadena decepcion en booleano
+x="decepcion"
+a=bool(x)
+print(a)
+
+#convertir el booleano 32<=67 en booleano
+x=32<=67
+a=bool(x)
+print(a)
+
+#convertir la cadena 75==68 en booleano
+x="75==68"
+a=bool(x)
+print(a)
