21fcde7f2dc19445ec398fa963615fa70c848413
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 19:37:44 -0500

    Create conversion-entero.py

---
+++ conversion-entero.py
@@ -0,0 +1,49 @@
+#convertir la cadena 1056 a entero
+x="1056"
+a=int(x)
+print(a)
+
+#convertir el flotante 212.08 a entero
+x=212.08
+a=int(x)
+print(a)
+
+#convertir el booleano 12>=6 a entero
+x=12>=6
+a=int(x)
+print(a)
+
+#convertir el booleano 72<=39 a entero
+x=72<=39
+a=int(x)
+print(a)
+
+#convertir el entero 121 a entero
+x=121
+a=int(x)
+print(a)
+
+#convertir la cadena -26 a entero
+x="-26"
+a=int(x)
+print(a)
+
+#convertir la cadena 1372 a entero
+x="1372"
+a=int(x)
+print(a)
+
+#convertir el flotante 172.16 a entero
+x=172.16
+a=int(x)
+print(a)
+
+#convertir el booleano 21==32 a entero
+x=21==32
+a=int(x)
+print(a)
+
+#convertir el booleano 249!=248 a entero
+x=249!=248
+a=int(x)
+print(a)
