4d11850d27ba7aa9ed9d36d3c2a51c688c183be2
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-04 19:49:53 -0500

    Create conversion-flotante.py

---
+++ conversion-flotante.py
@@ -0,0 +1,49 @@
+#convertir la cadena 1056.17 a flotante
+x="1056.17"
+a=float(x)
+print(a)
+
+#convertir el flotante 123.57 a flotante
+x=123.57
+a=float(x)
+print(a)
+
+#convertir el entero 81 a flotante
+x=81
+a=float(x)
+print(a)
+
+#convertir el booleano 99>=25 a flotante
+x=99>=25
+a=float(x)
+print(a)
+
+#convertir 119==658 a flotante
+x=119==658
+a=float(x)
+print(a)
+
+#convertir la cadena -126 a flotante
+x="-126"
+a=float(x)
+print(a)
+
+#convertir la cadena .87 a flotante
+x=".87"
+a=float(x)
+print(a)
+
+#convertir la cadena 1142.55 a flotante
+x="1142.55"
+a=float(x)
+print(a)
+
+#convertir el entero 176 a flotante
+x=176
+a=float(x)
+print(a)
+
+#convertir la cadena .625 a flotante
+x=".625"
+a=float(x)
+print(a)
