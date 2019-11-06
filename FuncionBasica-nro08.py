bf9f2c7ba6102eebd57c065299b076f689736d80
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 22:34:19 -0500

    Create FuncionBasica-nro08.py

---
+++ FuncionBasica-nro08.py
@@ -0,0 +1,18 @@
+#input
+pi=float(input("ingrese el valor de pi:"))
+semieje_menor=float(input("ingrese semieje menor:"))
+semieje_mayor=float(input("ingrese semieje mayor:"))
+
+#processing
+area_elipse=pi*semieje_mayor*semieje_menor
+
+#outpout
+print("##################################")
+print("#calcular el area de un elipse")
+print("##################################")
+print("#")
+print("##################################")
+print("#semieje_menor           :", semieje_menor)
+print("#semieje_mayor           :", semieje_mayor)
+print("#area                    :", area_elipse)
+print("##################################")
