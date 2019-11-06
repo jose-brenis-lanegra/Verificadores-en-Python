b326525b89967874420e32156e0966da20492c57
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 22:03:56 -0500

    Create FuncionBasica-nro06.py

---
+++ FuncionBasica-nro06.py
@@ -0,0 +1,19 @@
+#input
+base_h=float(input("ingrese la base h:"))
+apotema=float(input("ingrese la apotema:"))
+
+#porcessing
+perimetro_hexagono=6*base_h
+area_hexagono=(perimetro_hexagono*apotema)/2
+
+#outpout
+print("###########################################")
+print("#hallar el perimetro y area de un hexagono")
+print("###########################################")
+print("#")
+print("###########################################")
+print("#base                                :", base_h)
+print("#apotema                             :", apotema)
+print("#perimetro                           :", perimetro_hexagono)
+print("#area                                :", area_hexagono)
+print("###########################################")
