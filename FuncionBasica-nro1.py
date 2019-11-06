7b793d4ce58ab6dd01c250f18ccb94aa0196b19b
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 19:45:37 -0500

    Create FuncionBasica-nro1.py

---
+++ FuncionBasica-nro1.py
@@ -0,0 +1,17 @@
+#input
+base=float(input("ingrese base:"))
+altura=float(input("ingrese altura:"))
+
+#processing
+area_triangulo=(base*altura)/2
+
+#outpout
+print("###########################################")
+print("#hallar el valor del area del triangulo")
+print("###########################################")
+print("#")
+print("###########################################")
+print("#base                            :", base)
+print("#altura                          :", altura)
+print("#area                            :", area_triangulo)
+print("###########################################")
