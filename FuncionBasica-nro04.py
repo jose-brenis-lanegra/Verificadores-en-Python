5630e4251b63fa2fd8bffd9f9398459582299eeb
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 21:20:13 -0500

    Create FuncionBasica-nro04.py

---
+++ FuncionBasica-nro04.py
@@ -0,0 +1,16 @@
+#input
+base=float(input("ingrese base:"))
+altura=float(input("ingrese altura:"))
+
+#processing
+perimetro_romboide=2*base+2*altura
+
+#outpout
+print("###########################################")
+print("#hallar el perimetro del romboide")
+print("###########################################")
+print("#")
+print("###########################################")
+print("#base                                  :", base)
+print("#altura                                :", altura)
+print("#perimetro                             :", perimetro_romboide)
