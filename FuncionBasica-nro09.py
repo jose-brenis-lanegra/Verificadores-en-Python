7d952d75cf1f14d3049ca24928f3f5df5213441f
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 22:44:00 -0500

    Create FuncionBasica-nro09.py

---
+++ FuncionBasica-nro09.py
@@ -0,0 +1,21 @@
+#input
+lado_1=float(input("ingrese lado01:"))
+lado_2=float(input("ingrese lado02:"))
+lado_3=float(input("ingrese lado03:"))
+lado_4=float(input("ingrese lado04:"))
+
+#processing
+perimetro_cuadrilatero=lado_1+lado_2+lado_3+lado_4
+
+#outpout
+print("###########################################")
+print("#hallar el perimetro de un cuadrilatero")
+print("###########################################")
+print("#")
+print("###########################################")
+print("#primer_lado                      :", lado_1)
+print("#segundo_lado                     :", lado_2)
+print("#tercer_lado                      :", lado_3)
+print("#cuarto_lado                      :", lado_4)
+print("#perimetro                        :", perimetro_cuadrilatero)
+print("###########################################")
