7c607640d7367505789ec1b941eee639a62c2f8e
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 19:59:56 -0500

    Create FuncionBasica_nro02.py

---
+++ FuncionBasica_nro02.py
@@ -0,0 +1,17 @@
+#imput
+lado=float(input("ingrese el lado:"))
+
+#processing
+perimetro_cuadrado=4*lado
+area_cuadrado=lado**2
+
+#outpout
+print("#############################################")
+print("#hallar el perimetro y area de un cuadrado")
+print("#############################################")
+print("#")
+print("#############################################")
+print("#lado                                  :", lado)
+print("#perimetro                             :", perimetro_cuadrado)
+print("#area                                  :", area_cuadrado)
+print("#############################################")
