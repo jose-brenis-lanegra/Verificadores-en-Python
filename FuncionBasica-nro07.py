47461a7a0bcfc322a0bbe5044fd02522b1cf8b1b
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 22:13:59 -0500

    Create FuncionBasica-nro07.py

---
+++ FuncionBasica-nro07.py
@@ -0,0 +1,16 @@
+#input
+pi=float(input("ingrese el valor de pi:"))
+radio=float(input("ingrese radio:"))
+
+#processing
+area_circulo=pi*(radio**2)
+
+#outpout
+print("###############################")
+print("#hallar el area del circulo")
+print("###############################")
+print("#")
+print("###############################")
+print("#radio                 :", radio)
+print("#area                  :", area_circulo)
+print("###############################")
