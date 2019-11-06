23a7f3051cd03bbefffa76004bc40b7cdda93053
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 21:41:43 -0500

    Create FuncionBasica-nro05.py

---
+++ FuncionBasica-nro05.py
@@ -0,0 +1,19 @@
+#input
+base_mayor=float(input("ingrese base mayor:"))
+base_menor=float(input("ingrese base menor:"))
+altura=float(input("ingrese altura:"))
+
+#processing
+area_trapecio=(altura*(base_mayor+base_menor))/2
+
+#outpout
+print("##########################################")
+print("#hallar el area de un trapecio")
+print("##########################################")
+print("#")
+print("##########################################")
+print("#base_mayor                          :", base_mayor)
+print("#base_menor                          :", base_menor)
+print("#altura                              :",altura)
+print("#area                                :", area_trapecio)
+print("##########################################")
