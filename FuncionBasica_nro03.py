69665221b9bec07e58e61f18e2434787daf0bf52
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-05 21:10:53 -0500

    Create FuncionBasica_nro03.py

---
+++ FuncionBasica_nro03.py
@@ -0,0 +1,16 @@
+#input
+diagonal_menor=float(input("ingrese diagonal menor:"))
+diagonal_mayor=float(input("ingrese diagonal mayor:"))
+
+#processing
+area_rombo=(diagonal_mayor*diagonal_menor)/2
+
+#outpout
+print("############################################")
+print("#hallar el area del rombo")
+print("############################################")
+print("#")
+print("############################################")
+print("#diagonal_mayor                        :", diagonal_mayor)
+print("#diagonal_menor                        :", diagonal_menor)
+print("#area                                  :", area_rombo)
