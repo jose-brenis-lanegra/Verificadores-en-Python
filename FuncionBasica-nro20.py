097896bee01e18a8f834ab84c323679609d68a4a
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 22:06:08 -0500

    Create FuncionBasica-nro20.py

---
+++ FuncionBasica-nro20.py
@@ -0,0 +1,19 @@
+#input
+a=float(input("ingrese a:"))
+b=float(input("ingrese b:"))
+c=float(input("ingrese c:"))
+
+#processing
+discriminante=(b**2)-(4*a*c)
+
+#outpout
+print("###############################################")
+print("#hallar la discriminante de una ecuacion")
+print("###############################################")
+print("#")
+print("###############################################")
+print("#a                                :", a)
+print("#b                                :", b)
+print("#c                                :", c)
+print("#discriminante                    :", discriminante)
+print("###############################################")
