439e63f7337c67fc7a37e05595c539ed119ac3f8
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 19:55:36 -0500

    Create FuncionBasica-nro12.py

---
+++ FuncionBasica-nro12.py
@@ -0,0 +1,16 @@
+#input
+pi=float(input("ingrese el valor de pi:"))
+radio=float(input("ingrese radio:"))
+
+#processing
+volumen_esfera=(4*pi*(radio**3))/3
+
+#outpout
+print("###################################")
+print("#hallar el volumen de una esfera")
+print("###################################")
+print("#")
+print("###################################")
+print("#radio                      :", radio)
+print("#volumen                    :", volumen_esfera)
+print("###################################")
