445bf865551850349eaa91697d5b6671c5d2778f
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 21:55:58 -0500

    Create FuncionBasica-nro19.py

---
+++ FuncionBasica-nro19.py
@@ -0,0 +1,17 @@
+#input
+arista=float(input("ingrese arista:"))
+
+#processing
+area_icosaedro=5*(3**(1/2))*(arista**2)
+volumen_icosaedro=(5*(3+(5**(1/2)))*(arista**3))/12
+
+#outpout
+print("#################################################")
+print("#hallar el area y volumen de un icosaedro")
+print("#################################################")
+print("#")
+print("#################################################")
+print("#arista                         :", arista)
+print("#area                           :", area_icosaedro)
+print("#volumen                        :", volumen_icosaedro)
+print("################################################")
