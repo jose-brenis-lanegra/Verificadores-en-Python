b26a2a1c12c024397fccc54cc831dd96813fa645
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 20:04:53 -0500

    Create FuncionBasica-nro13.py

---
+++ FuncionBasica-nro13.py
@@ -0,0 +1,18 @@
+#input
+pi=float(input("ingrese el valor de pi"))
+altura=float(input("ingrese la altura:"))
+radio=float(input("ingrese la radio:"))
+
+#processing
+area_total_cilindro=2*pi*radio*(radio + altura)
+
+#outpout
+print("#####################################")
+print("#hallar el area total del cilindro")
+print("#####################################")
+print("#")
+print("#####################################")
+print("#altura                     :", altura)
+print("#radio                      :", radio)
+print("#area_total                 :", area_total_cilindro)
+print("#####################################")
