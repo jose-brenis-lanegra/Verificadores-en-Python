b93054b7e8684c702d939e9220cc52e28c691ec7
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 21:27:57 -0500

    Create FuncionBasica-nro16.py

---
+++ FuncionBasica-nro16.py
@@ -0,0 +1,17 @@
+#input
+arista=float(input("ingrese arista:"))
+
+#processing
+altura=((6**(1/2))*arista)/3
+volumen_tetraedro=((2**(1/2))*(arista**3))/12
+
+#outpout
+print("################################################")
+print("#hallar la altura y volumen de un tetraedro")
+print("################################################")
+print("#")
+print("################################################")
+print("#arista                               :", arista)
+print("#altura                               :", altura)
+print("#volumen                              :", volumen_tetraedro)
+print("###############################################")
