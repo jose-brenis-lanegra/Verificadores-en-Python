313af093c71c5b6449ba64dd1ab7806be154a6e0
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 21:35:43 -0500

    Create FuncionBasica-nro17.py

---
+++ FuncionBasica-nro17.py
@@ -0,0 +1,17 @@
+#input
+arista=float(input("ingrese arista:"))
+
+#processing
+area_octaedro=2*(3**(1/2))*(arista**2)
+volumen_octaedro=((2**(1/2))*(arista**3))/3
+
+#outpout
+print("################################################")
+print("#hallar el area y el volumen de un octaedro")
+print("################################################")
+print("#")
+print("################################################")
+print("#arista                                :", arista)
+print("#area                                  :", area_octaedro)
+print("#volumen                               :", volumen_octaedro)
+print("###############################################")
