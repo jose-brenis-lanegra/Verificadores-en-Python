38a467d034a992e00598f04cb3097784c000310d
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 19:33:17 -0500

    Create FuncionBasica-nro10.py

---
+++ FuncionBasica-nro10.py
@@ -0,0 +1,17 @@
+#input
+arista=float(input("ingrese arista:"))
+
+#processing
+area_cubo=6*(arista**2)
+volumen_cubo=arista**3
+
+#outpout
+print("#######################################")
+print("#hallar el area y volumen de un cubo")
+print("#######################################")
+print("#")
+print("#######################################")
+print("#arista                       :", arista)
+print("#area                         :", area_cubo)
+print("#volumen                      :", volumen_cubo)
+print("#######################################")
