cfaadbb933e6c75bf096d7d1d1038f334d429176
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 21:19:33 -0500

    Create FuncionBasica-nro15.py

---
+++ FuncionBasica-nro15.py
@@ -0,0 +1,18 @@
+#input
+pi=float(input("ingrese el valor de pi:"))
+arista=float(input("ingrese arista:"))
+altura=float(input("ingrese altura:"))
+
+#processing
+volumen_piramide=(pi(radio**2)*h)/3
+
+#outpout
+print("#######################################")
+print("#hallar el volumen de una piramide")
+print("#######################################")
+print("#")
+print("#######################################")
+print("#arista                       :", arista)
+print("#altura                       :", altura)
+print("#volumen                      :", volumen_piramide)
+print("#######################################")
