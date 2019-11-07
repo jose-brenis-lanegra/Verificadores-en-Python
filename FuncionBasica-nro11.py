e96d20777c195a6bbb756777e3474041a5813c07
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-06 19:46:35 -0500

    Create FuncionBasica-nro11.py

---
+++ FuncionBasica-nro11.py
@@ -0,0 +1,19 @@
+#input
+largo=float(input("ingrese largo:"))
+ancho=float(input("ingrese ancho:"))
+altura=float(input("ingrese altura:"))
+
+#processing
+volumen_prisma=largo*ancho*altura
+
+#outpout
+print("###############################")
+print("#hallar el volumen del prisma")
+print("###############################")
+print("#")
+print("###############################")
+print("#largo                  :", largo)
+print("#ancho                  :", ancho)
+print("#altura                 :", altura)
+print("#volumen                :", volumen_prisma)
+print("###############################")
